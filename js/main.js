aa32573e755a4de820a0de963ee9bd8e64f9a66b
Aleksei Petrakov <tppd67421@gmail.com>
2021-04-25 03:03:34 +0300

    edit layout and add filters

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
-window.onload = function() {
+window.onload = function () {
 
     const anchors = document.querySelectorAll('a[href*="#"]')
 
     for (let anchor of anchors) {
         anchor.addEventListener('click', function (e) {
             e.preventDefault()
-            const blockID = anchor.getAttribute('href')  
-            document.querySelector('' + blockID).scrollIntoView( {
+            const blockID = anchor.getAttribute('href')
+            document.querySelector('' + blockID).scrollIntoView({
                 behavior: 'smooth',
                 block: 'start'
             })
         });
+    }
+
+
+    const type = document.querySelector('.type')
+    if (type) {
+        const typeButtons = type.querySelectorAll('.type__button')
+        const portfolioList = document.querySelectorAll('.portfolio__item')
+        typeButtons.forEach(button => button.addEventListener('click', applyFilter))
+
+        function applyFilter(e) {
+            const currentFilter = e.target.getAttribute('data-filter')
+
+            portfolioList.forEach(portfolioItem => {
+                if (currentFilter === 'all') {
+                    portfolioItem.classList.remove('hidden')
+                } else {
+                    if (portfolioItem.getAttribute('data-filter') !== currentFilter) {
+                        portfolioItem.classList.add('hidden')
+                    } else {
+                        portfolioItem.classList.remove('hidden')
+                    }
+                }
+            })
+
+            typeButtons.forEach(button => button.classList.remove('active'))
+            e.target.classList.add('active')
+        }
     }
 
 };
